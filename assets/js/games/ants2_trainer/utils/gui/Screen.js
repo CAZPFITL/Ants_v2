6a02232120ca2dc0c8a3297cba8ad64a5fc602a8
caz <cazpfitl@gmail.com>
2022-08-22 02:09:30 -0500

    feat: v1.7.88

--- assets/js/games/ants2_trainer/utils/gui/Screen.js
+++ assets/js/games/ants2_trainer/utils/gui/Screen.js
@@ -22,6 +22,7 @@ export default class Screen {
         this.buttonsCollection = {};
         this.abstractStates = {
             creating: false,
+            looping: false,
         }
         this.updateExtend = () => {
             if (this.app.game.state.state === PLAY && this.app.game.level) {
@@ -45,21 +46,214 @@ export default class Screen {
                     }
                 }
 
-                //CREATING TRACE PROCESS (ONLY INSIDE THE MAP)
-                if (this.buttonsStates.trace) {
-                    const entity = {
-                        x: (-this.app.game.level.size.width) / 2,
-                        y: (-this.app.game.level.size.height) / 2,
-                        width: this.app.game.level.size.width,
-                        height: this.app.game.level.size.height
+                //CREATING TRACE PROCESS
+                // if (this.buttonsStates.createTrace) {
+                //     const entity = {
+                //         x: (-this.app.game.level.size.width) / 2,
+                //         y: (-this.app.game.level.size.height) / 2,
+                //         width: this.app.game.level.size.width,
+                //         height: this.app.game.level.size.height
+                //     }
+                //     if (this.app.gui.get.isHover(entity, hoverTranslatedCoords)) {
+                //         this.app.factory.binnacle['Traces'][0].markTrace(hoverTranslatedCoords);
+                //     }
+                // }
+
+                // DRAG RESIZE WIDTH
+                if (this.buttonsStates.width === 'click') {
+                    this.app.game.level.size.width += e.movementX;
+                    this.app.game.level.coords.x = -this.app.game.level.size.width / 2 ?? 100;
+                }
+
+                // DRAG RESIZE HEIGHT
+                if (this.buttonsStates.height === 'click') {
+                    const increment = e.movementX;
+                    this.app.game.level.size.height += increment;
+                    this.app.game.level.coords.y = -this.app.game.level.size.height / 2 ?? 100;
+                }
+
+                // DRAG MIN TRACE - (click, drag and drop)
+                if (this.buttonsStates.minTrace) {
+                    if (this.app.factory.binnacle['Traces'] && this.app.factory.binnacle['Traces'][0]) {
+                        const ref = this.app.factory.binnacle['Traces'][0].props.min;
+                        const refMax = this.app.factory.binnacle['Traces'][0].props.max;
+                        if (ref >= 1 && ref <= refMax) {
+                            this.app.factory.binnacle['Traces'][0].props.min += e.movementX;
+                        }
+                        if (ref > refMax) {
+                            this.app.factory.binnacle['Traces'][0].props.min = refMax;
+                        }
+                        if (ref < 1) {
+                            this.app.factory.binnacle['Traces'][0].props.min = 1;
+                        }
                     }
-                    if (this.app.gui.get.isHover(entity, hoverTranslatedCoords) && this.app.factory.binnacle['Traces'][0]) {
-                        this.app.factory.binnacle['Traces'][0].markTrace(hoverTranslatedCoords);
+                }
+
+                // DRAG UPDATE MAX TRACE - (click, drag and drop)
+                if (this.buttonsStates.maxTrace) {
+                    if (this.app.factory.binnacle['Traces'] && this.app.factory.binnacle['Traces'][0]) {
+                        const ref = this.app.factory.binnacle['Traces'][0].props.max;
+                        const refMin = this.app.factory.binnacle['Traces'][0].props.min;
+                        if (ref >= refMin && ref <= 30) {
+                            this.app.factory.binnacle['Traces'][0].props.max += e.movementX;
+                        }
+                        if (ref > 30) {
+                            this.app.factory.binnacle['Traces'][0].props.max = 30;
+                        }
+                        if (ref < refMin) {
+                            this.app.factory.binnacle['Traces'][0].props.max = refMin;
+                        }
+                    }
+                }
+
+                // DRAG UPDATE LOOP SIZE - (click, drag and drop)
+                if (this.buttonsStates.loopSize) {
+                    const ref = this.app.game.flags.antLooper;
+                    const refMax = 3000
+                    const refMin = 20;
+                    if (ref >= refMin && ref <= refMax) {
+                        this.app.game.flags.antLooper += e.movementX;
+                    }
+                    if (ref > refMax) {
+                        this.app.game.flags.antLooper = refMax;
+                    }
+                    if (ref < refMin) {
+                        this.app.game.flags.antLooper = refMin;
+                    }
+                }
+
+            },
+            mouseup: (e) => {
+                // CLEAR STATES
+                this.buttonsStates.createAnt = 'normal';
+                this.buttonsStates.createAnthill = 'normal';
+                this.buttonsStates.createFood = 'normal';
+                this.buttonsStates.createTrace = 'normal';
+                this.buttonsStates.width = 'normal';
+                this.buttonsStates.height = 'normal';
+                this.buttonsStates.minTrace = 'normal';
+                this.buttonsStates.maxTrace = 'normal';
+                this.buttonsStates.loopSize = 'normal';
+                this.buttonsStates.createLoop = 'normal';
+                this.buttonsStates.saveNetworks = 'normal';
+
+                if (!this.abstractStates.creating) {
+                    // CREATE TRACE
+                    this.app.gui.get.isClicked(
+                        this.buttonsCollection.PLAY.createTrace,
+                        {x: e.offsetX, y: e.offsetY},
+                        () => {
+                            this.buttonsStates.createTrace = !this.buttonsStates.createTrace
+                        }
+                    )
+
+                    // CLEAR NETWORKS
+                    this.app.gui.get.isClicked(
+                        this.buttonsCollection.PLAY.clearNetworks,
+                        {x: e.offsetX, y: e.offsetY},
+                        () => {
+                            this.buttonsStates.clearing = 0;
+                            localStorage.removeItem('_best');
+                        }
+                    )
+                    // SAVE NETWORKS
+                    this.app.gui.get.isClicked(
+                        this.buttonsCollection.PLAY.saveNetworks,
+                        {x: e.offsetX, y: e.offsetY},
+                        () => {
+                            if ((this.app.factory.binnacle?.Anthill?.length ?? 0) > 0) {
+                                this.buttonsStates.saveNetworks = 'normal';
+                                Parser.save(this.app.factory.binnacle.Ant);
+                            }
+                        }
+                    )
+                    // LOAD FROM NETWORKS
+                    this.app.gui.get.isClicked(
+                        this.buttonsCollection.PLAY.loadFromNetworks,
+                        {x: e.offsetX, y: e.offsetY},
+                        () => {
+                            if ((this.app.factory.binnacle?.Anthill?.length ?? 0) > 0) {
+                                this.buttonsStates.loadFromNetworks = !this.buttonsStates.loadFromNetworks;
+                                if(this.buttonsStates.loadFromNetworks) {
+                                    this.app.game.LOADED_BRAINS = Parser.load();
+                                    window.a = this.app.game.LOADED_BRAINS;
+                                    window.b = this.app.player.ant;
+                                } else {
+                                    this.app.game.LOADED_BRAINS = false;
+                                }
+                            }
+                        }
+                    )
+                    // OSCILLATE MAP
+                    // this.app.gui.get.isClicked(
+                    //     this.buttonsCollection.PLAY.oscillateMap,
+                    //     {x: e.offsetX, y: e.offsetY},
+                    //     () => {
+                    //         this.buttonsStates.oscillating = !this.buttonsStates.oscillating;
+                    //     }
+                    // )
+                    if (!this.buttonsStates.createTrace && !this.abstractStates.looping) {
+                        // CREATE ANTHILL - (mouseup)
+                        this.app.gui.get.isClicked(
+                            this.buttonsCollection.PLAY.createAnthill,
+                            {x: e.offsetX, y: e.offsetY},
+                            () => {
+                                this.abstractStates.creating = true;
+                                this.buttonsStates.createAnthill = true;
+                                this.app.game.level.Anthill({ants: 0, free: true});
+                                this.creation = this.app.factory.binnacle.Anthill[this.app.factory.binnacle.Anthill.length - 1];
+                            }
+                        )
+
+                        // CREATE FOOD - (mouseup)
+                        this.app.gui.get.isClicked(
+                            this.buttonsCollection.PLAY.createFood,
+                            {x: e.offsetX, y: e.offsetY},
+                            () => {
+                                this.abstractStates.creating = true;
+                                this.buttonsStates.createFood = true
+                                this.app.game.level.Food({amount: 1});
+                                this.creation = this.app.factory.binnacle.Food[this.app.factory.binnacle.Food.length - 1];
+                            }
+                        )
+
+                        // CREATE ANT - (mouseup)
+                        if (this.app.player.anthill) {
+                            this.app.gui.get.isClicked(
+                                this.buttonsCollection.PLAY.createAnt,
+                                {x: e.offsetX, y: e.offsetY},
+                                () => {
+                                    this.app.player.anthill.addAnt();
+                                }
+                            )
+                        }
+                    }
+                    this.buttonsStates.clearing = false;
+
+                } else {
+                    const objX = (this.creation?.size?.width ?? this.creation.width);
+                    const objY = (this.creation?.size?.height ?? this.creation.height);
+                    const entity = {
+                        x: (-this.app.game.level.size.width + objX) / 2,
+                        y: (-this.app.game.level.size.height + objY) / 2,
+                        width: this.app.game.level.size.width - objX,
+                        height: this.app.game.level.size.height - objY
+                    }
+                    const click = this.app.gui.get.viewportCoords(
+                        {x: e.offsetX, y: e.offsetY},
+                        this.app.camera.viewport
+                    );
+                    if (this.app.gui.get.isHover(entity, click)) {
+                        this.creation = null;
+                        this.abstractStates.creating = false;
+                        this.buttonsStates.createAnthill = false;
+                        this.buttonsStates.createFood = false;
                     }
                 }
             },
-            mouseup: (e) => true,
-            mousedown: (e) => true,
+            mousedown: (e) => {
+
+            },
             click: (e) => true,
         });
     }
@@ -92,12 +286,9 @@ export default class Screen {
             }, this.app.camera.viewport));
         });
         this.app.controls.pushListener(this, 'mouseup', (event) => {
-            const buttons = {
-                ...this.buttonsCollection.MAIN_MENU,
-                ...this.buttonsCollection.PLAY
-            }
+            const buttons = this.#getButtons()
 
-            Object.keys(buttons).forEach(key => {
+            Object.keys(buttons).forEach((key) => {
                 const ctx = buttons[key].props.ctx === this.app.gui.ctx
                     ? this.app.gui.get.clickCoords(event, this.app.camera.viewport)
                     : {x: event.offsetX, y: event.offsetY};
@@ -111,12 +302,9 @@ export default class Screen {
             abstractEvents.mouseup(event);
         });
         this.app.controls.pushListener(this, 'mousedown', (event) => {
-            const buttons = {
-                ...this.buttonsCollection.MAIN_MENU,
-                ...this.buttonsCollection.PLAY
-            }
+            const buttons = this.#getButtons()
 
-            Object.keys(buttons).forEach(key => {
+            Object.keys(buttons).forEach((key) => {
                 const ctx = buttons[key].props.ctx === this.app.gui.ctx
                     ? this.app.gui.get.clickCoords(event, this.app.camera.viewport)
                     : {x: event.offsetX, y: event.offsetY};
@@ -130,12 +318,9 @@ export default class Screen {
             abstractEvents.mousedown(event);
         });
         this.app.controls.pushListener(this, 'click', (event) => {
-            const buttons = {
-                ...this.buttonsCollection.MAIN_MENU,
-                ...this.buttonsCollection.PLAY
-            }
+            const buttons = this.#getButtons()
 
-            Object.keys(buttons).forEach(key => {
+            Object.keys(buttons).forEach((key) => {
                 const ctx = buttons[key].props.ctx === this.app.gui.ctx
                     ? this.app.gui.get.clickCoords(event, this.app.camera.viewport)
                     : {x: event.offsetX, y: event.offsetY};
@@ -318,8 +503,7 @@ export default class Screen {
                                     this.app.game.level.Food({amount: 1});
                                     this.creation = this.app.factory.binnacle.Food[this.app.factory.binnacle.Food.length - 1];
                                 }
-                            },
-                            mouseup: () => this.buttonsStates.createFood = 'normal'
+                            }
                         }
                     }
                 },
@@ -340,11 +524,10 @@ export default class Screen {
                         stroke: this.colors.MAIN_MENU.buttons.variation1.stroke,
                         widthStroke: 2,
                         callbacks: {
-                            click: (e) => {
-                                console.log('adsasd');
-                                this.buttonsStates.createTrace = this.buttonsStates.createTrace === 'normal' ? 'click' : 'normal'
-                                console.log(e)
-                            },
+                            click: (e) =>
+                                this.buttonsStates.createTrace = this.buttonsStates.createTrace === 'normal'
+                                    ? 'click'
+                                    : 'normal'
                         }
                     }
                 },
@@ -365,8 +548,7 @@ export default class Screen {
                         stroke: this.colors.MAIN_MENU.buttons.variation1.stroke,
                         widthStroke: 2,
                         callbacks: {
-                            click: () => console.log('width'),
-                            mouseup: () => this.buttonsStates.height = 'normal'
+                            mousedown: () => this.buttonsStates.width = 'click'
                         }
                     }
                 },
@@ -387,8 +569,7 @@ export default class Screen {
                         stroke: this.colors.MAIN_MENU.buttons.variation1.stroke,
                         widthStroke: 2,
                         callbacks: {
-                            click: () => console.log('height'),
-                            mouseup: () => this.buttonsStates.height = 'normal'
+                            mousedown: () => this.buttonsStates.height = 'click'
                         }
                     }
                 },
@@ -414,7 +595,6 @@ export default class Screen {
                         widthStroke: 2,
                         callbacks: {
                             mousedown: () => this.buttonsStates.minTrace = 'click',
-                            mouseup: () => this.buttonsStates.minTrace = 'normal'
                         }
                     }
                 },
@@ -439,8 +619,7 @@ export default class Screen {
                         stroke: this.colors.MAIN_MENU.buttons.variation1.stroke,
                         widthStroke: 2,
                         callbacks: {
-                            mousedown: () => this.buttonsStates.maxTrace = 'click',
-                            mouseup: () => this.buttonsStates.maxTrace = 'normal'
+                            mousedown: () => this.buttonsStates.maxTrace = 'click'
                         }
                     }
                 },
@@ -463,8 +642,7 @@ export default class Screen {
                         stroke: this.colors.MAIN_MENU.buttons.variation1.stroke,
                         widthStroke: 2,
                         callbacks: {
-                            mousedown: () => this.buttonsStates.loopSize = 'click',
-                            mouseup: () => this.buttonsStates.loopSize = 'normal'
+                            mousedown: () => this.buttonsStates.loopSize = 'click'
                         }
                     }
                 },
@@ -489,7 +667,9 @@ export default class Screen {
                         callbacks: {
                             mousedown: () => {
                                 if ((this.app.factory.binnacle?.Anthill?.length ?? 0) > 0) {
-                                        this.buttonsStates.createLoop = this.buttonsStates.createLoop === 'click' ? 'normal' : 'click';
+                                        this.buttonsStates.createLoop = this.buttonsStates.createLoop === 'click'
+                                            ? 'normal'
+                                            : 'click';
                                 }
                             }
                         }
@@ -519,8 +699,7 @@ export default class Screen {
                                     this.buttonsStates.saveNetworks = 'click';
                                     Parser.save(this.app.factory.binnacle.Ant);
                                 }
-                            },
-                            mouseup: () => this.buttonsStates.saveNetworks = 'normal'
+                            }
                         }
                     }
                 },
@@ -580,8 +759,7 @@ export default class Screen {
                             mousedown: () => {
                                 this.buttonsStates.clearNetworks = 'click';
                                 localStorage.removeItem('_best');
-                            },
-                            mouseup: () => this.buttonsStates.clearNetworks = 'normal'
+                            }
                         }
                     }
                 }
